536dc4cca4438eb2285c89a558ecea7bcd2e6e3f
Agung Pramono <agungpermadi13@gmail.com>
2015-11-13 09:18:51 +0700

    Class untuk akses database

---
+++ src/main/java/com/agung/template/springjpa/dao/ProdukDao.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (c) 2015 Agung Pramono <agungpermadi13@gmail.com || www.github.com/agung pramono>.
+ *  All rights reserved.
+ * 
+ * Silahkan digunakan dengan bebas / dimodifikasi
+ * Dengan tetap mencantumkan nama @author dan Referensi / Source
+ * Terima Kasih atas Kerjasamanya.
+ * 
+ *  ProdukDao.java
+ * 
+ *  Created on Nov 13, 2015, 8:46:44 AM
+ */
+package com.agung.template.springjpa.dao;
+
+import com.agung.template.springjpa.entity.Produk;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+/**
+ *
+ * @author agung
+ */
+public interface ProdukDao extends PagingAndSortingRepository<Produk, String>{
+    //contoh query method
+    Produk findByKodeProduk(String kode);
+    
+    //contoh query creation
+    @Query("select p from Produk p "
+            + "where lower(p.kodeProduk) like lower(:search) "
+            + "or lower(p.namaProduk) like lower(:search) "
+            + "order by p.namaProduk asc")
+    Page<Produk>search(
+            @Param("search")String search,
+            Pageable page);
+}
