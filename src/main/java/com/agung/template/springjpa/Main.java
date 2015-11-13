fb5d09e6db913b329f266dbdf561727bf69d83ad
Agung Pramono <agungpermadi13@gmail.com>
2015-11-13 09:17:28 +0700

    Main class program

---
+++ src/main/java/com/agung/template/springjpa/Main.java
@@ -0,0 +1,25 @@
+package com.agung.template.springjpa;
+
+import com.agung.template.springjpa.service.ProdukService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+
+    private static ApplicationContext springContext;
+    private static ProdukService produkService;
+
+    private static void initContext() {
+        springContext = new ClassPathXmlApplicationContext("classpath*:applicationContext.xml");
+        produkService = (ProdukService) springContext.getBean("produkService");
+    }
+
+    public static void main(String[] args) {
+        initContext();
+    }
+
+}
